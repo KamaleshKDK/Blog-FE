c78ccd46dee22754d08e65fc2aada337fac06edb
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-21 16:05:30 +0530

    use navigate added

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Register.css'
 
+
 function Register() {
+    const navigate = useNavigate()
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -18,7 +20,8 @@ function Register() {
                 email,
                 password,
             });
-            res.data && window.location.replace("/");
+            res.data && navigate("/login")
+            //    window.location.replace("/login");
         } catch (err) {
             setError(true);
         }
